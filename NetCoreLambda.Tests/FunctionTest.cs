dedac33220f51ffbaed7ae1f318a71422631cc8a
joao luis baptista de almeida <jlbacs@hotmail.com>
2019-09-17 19:31:53 -0300

    Added the required package references to unit test project. Added the skip attribute to the actual implementation unit test to remove it from CI/CD pipeline.

--- NetCoreLambda.Tests/FunctionTest.cs
+++ NetCoreLambda.Tests/FunctionTest.cs
@@ -7,8 +7,11 @@ namespace NetCoreLambda.Tests
 {
     public class FunctionTest
     {
+        /// <summary>
+        /// Mocked unit test used in CI/CD pipeling
+        /// </summary>
         [Fact]
-        public void Function_Should_Return_Config_Variable()
+        public void Function_Should_Return_Config_Variable_Mocked()
         {
             // Mock IConfiguration
             var expected = "val1";
@@ -22,6 +25,22 @@ namespace NetCoreLambda.Tests
             // Invoke the lambda function and confirm config value is returned
             var function = new Function(mockConfigService.Object);
             var result = function.FunctionHandler("env1", new TestLambdaContext());
+            Assert.Equal(expected, result);
+        }
+
+        /// <summary>
+        /// Actual implementation unit test skipped from CI/CD
+        /// </summary>
+        [Fact(Skip = "Remove implementation unit test from CI/CD pipeline")]
+        public void Function_Should_Return_Config_Variable()
+        {
+            var expected = "val1";
+
+            // Invoke the lambda function and confirm config value is returned
+            var function = new Function();
+
+            var result = function.FunctionHandler("env1", new TestLambdaContext());
+
             Assert.Equal(expected, result);
         }
     }
